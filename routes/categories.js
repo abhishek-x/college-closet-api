051c4567055fa04344711fb5fc1e97fe17c9e691
Abhishek Aggarwal <abhishek.aggarwal836@gmail.com>
2023-04-26 12:03:18 +0530

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const categoryController = require("../controller/categories");
 const multer = require("multer");
+const path = require('path');
 const { loginCheck } = require("../middleware/auth");
 
 // Image Upload setting
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/uploads/categories");
+    cb(null, path.join(__dirname, '/public/uploads/categories'));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "_" + file.originalname);
